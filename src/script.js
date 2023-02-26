6f9f9457fc9656b2774a5ab721bc851edad8877f
jenishngl <9320926+jenishngl@users.noreply.github.com>
2023-02-26 14:14:33 +0000

    Updated calculation logic

--- src/script.js
+++ src/script.js
@@ -1,7 +1,22 @@
 const form = document.getElementById("calculator-form");
 
+const gender = document.getElementById("gender")
+const height = document.getElementById("height")
+const weight = document.getElementById("weight")
+const age = document.getElementById("age")
+const activity = document.getElementById("activity")
+const lost_weight = document.getElementById("lost_weight")
+const calories = document.getElementById("calories")
+
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
+    console.log(gender.value, height.value, weight.value, age.value, activity.value, lost_weight.value)
+    if (gender.value === "M"){
+        bmr = 10 * parseFloat(weight.value) + 6.25 * parseFloat(height.value) - 5 * parseFloat(age.value) + 5
+        calories.value = parseFloat(bmr) * parseFloat(activity.value)
+    }else if (gender.value === "F"){
+        bmr = (10 * 83) + (6.25 * 172) - 5 * 28 + 5
+    }else{}
 })
 
 if ('serviceWorker' in navigator){
