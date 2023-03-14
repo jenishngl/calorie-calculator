e7126f08cd50fda15ff5bae053d32b02efbd2eb7
jenishngl <9320926+jenishngl@users.noreply.github.com>
2023-03-14 16:59:49 +0000

    Added more details results

--- src/script.js
+++ src/script.js
@@ -7,17 +7,25 @@ const age = document.getElementById("age")
 const activity = document.getElementById("activity")
 const lost_weight = document.getElementById("lost_weight")
 const calories = document.getElementById("calories")
+const protein = document.getElementById("protein")
+const carbs = document.getElementById("carbs")
+const fat = document.getElementById("fat")
+const results = document.getElementById("results")
 
 form.addEventListener("submit", (e) =>{
     e.preventDefault();
     console.log(gender.value, height.value, weight.value, age.value, activity.value, lost_weight.value)
     if (gender.value === "M"){
         bmr = 10 * parseFloat(weight.value) + 6.25 * parseFloat(height.value) - 5 * parseFloat(age.value) + 5
-        calories.value = parseFloat(bmr) * parseFloat(activity.value)
     }else if (gender.value === "F"){
         bmr = 10 * parseFloat(weight.value) + 6.25 * parseFloat(height.value) - 5 * parseFloat(age.value) - 161
-        calories.value = parseFloat(bmr) * parseFloat(activity.value)
     }else{}
+    calorie_val = parseFloat(bmr) * parseFloat(activity.value)
+    calories.textContent = calorie_val + " kcal/day"
+    protein.textContent = ((calorie_val * 24.5) / (100 * 4)).toFixed(2) + " g"
+    carbs.textContent = ((calorie_val * 52) / (100 * 4)).toFixed(2) + " g"
+    fat.textContent = ((calorie_val * 23.5) / (100 * 9)).toFixed(2) + " g"
+    results.classList.toggle("invisible")
 })
 
 if ('serviceWorker' in navigator){
--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "globDirectory": "src/",
     "globPatterns": [
-      "**/*.{html,js,css,svg,webp,webmanifest}"
+      "**/*.{html,js,css,png,svg,webp,webmanifest}"
     ],
     "swDest": "src/sw.js",
     "skipWaiting": true,
